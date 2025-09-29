5911d4d7eaf85b0bd579fe810e2ae70367cd824a
Alexander Douieb <adouieb@f1r10s14.clusters.42paris.fr>
2025-09-29 19:21:37 +0200

    added all function in utils.h file

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: adouieb <adouieb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:57:41 by adouieb           #+#    #+#             */
-/*   Updated: 2025/09/29 18:18:10 by adouieb          ###   ########.fr       */
+/*   Updated: 2025/09/29 19:20:40 by adouieb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int		ft_atoi(char *str);
+
 void	ft_free_str(char **str_addr);
 void	ft_free_board(char ***board_addr, int rows);
+
 void	ft_putstr(char *str);
+
+char	is_in_charset(char c, char *charset);
+int		word_count(char *str, char *charset);
+void	ft_multistrdup(char **result, char* str, char *charset);
+void	ft_multistrdup(char **result, char* str, char *charset);
 char	**ft_split(char *str, char *charset);
+
 char	*ft_strcpycat(char *prev, char *new, int size);
-char	*ft_strndup(char *str, int size);
+
 int		ft_strlen(char *str);
+
 char	*ft_strncat(char *dest, char *src, int size);
+
 char	*ft_strncpy(char *dest, char *src, int size);
+
+char	*ft_strndup(char *str, int size);
 
 #endif
