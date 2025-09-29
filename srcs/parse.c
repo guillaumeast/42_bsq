e8199aa92068008a60e98939249e9cf2b18b69b8
Guillaume Astesana <gastesan@f1r10s15.clusters.42paris.fr>
2025-09-30 00:04:29 +0200

    coupure courant

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: adouieb <adouieb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:31:30 by adouieb           #+#    #+#             */
-/*   Updated: 2025/09/29 21:35:16 by gastesan         ###   ########.fr       */
+/*   Updated: 2025/09/30 00:03:27 by gastesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,17 +37,22 @@ char	check_rules(char *str)
 }
 
 // Caller must free t_rules
-char get_rules(char *str, t_rules **rules)
+t_run	get_rules(char *str, t_run run)
 {
-	t_rules	*rules;
 	int		size;
 	char	*number;
 
 	if (!check_rules)
+	{
+		// free + null + error
 		return (0);
+	}
 	rules = malloc(sizeof(t_rules));
 	if (!rules)
+	{
+		// free + null + error
 		return (0);
+	}
 	size = ft_strlen(str);
 	number = strndup(str, size - 3);
 	if (!number)
@@ -65,23 +70,61 @@ char get_rules(char *str, t_rules **rules)
 	rules->empty = str[size - 3];
 	rules->obstacle = str[size - 2];
 	rules->filled = str[size - 1];
-	return (rules);
+	return (1);
 }
 
-t_exercice	parse(t_exercice)
+char	check_board(char **board, t_rules rules)
+{
+	int		i;
+	int		len;
+	char	*charset;
+	
+	// Au moins une ligne d'au moins une case
+	if (!board[0] || !board[0][0])
+		return (0)
+	i = 0;
+	len = ft_strlen(board[0]);
+	charset[0] = rules.empty;
+	charset[1] = rules.obstacle;
+	charset[2] = '\0';
+	while(board[i])
+	{
+		// Toutes les lignes ont la meme taille
+		if (ft_strlen(board[i] != len))
+			return (0);
+		// Contient uniquement les caracteres du charset
+		j = 0;
+		while (board[i][j])
+		{
+			if (!is_in_charset(board[i][j], charset))
+				return (0);
+			j++;
+		}	
+	}
+}
+
+t_run	parse(t_run run)
 {
 	char	**lines;
-	int		i;
 
 	lines = ft_split(input, "\n");
 	if (!lines)
-		return (NULL);
-	if (!get_rules(lines[0], &rules))
 	{
-		ft_free_list(&lines, -1)
-		return (NULL);
+		// Set run to NULL/ERR
+		return (run);
 	}
-	i = 1;
+	if (!get_rules(lines[0], rune->&rules))
+	{
+		ft_free_str_list(&lines, -1)
+		// Set run to NULL/ERR
+		return (run);
+	}
+	if (!check_board(lines + 1))
+	{
+		// Set run to NULL/ERR
+		return (run);
+	}
 	// check each line validity
+	// create board (copy pointers)
 	// return 0 + struct NULL if fail || 1 + struct if success
 }
