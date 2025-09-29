52e55a870890101cec551c495dfe9c3891ff9b59
Guillaume Astesana <gastesan@f1r10s15.clusters.42paris.fr>
2025-09-29 17:18:06 +0200

    modified parse.h struct

--- includes/parse.h
+++ includes/parse.h
@@ -6,18 +6,19 @@
 /*   By: adouieb <adouieb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:47:18 by adouieb           #+#    #+#             */
-/*   Updated: 2025/09/29 14:20:04 by adouieb          ###   ########.fr       */
+/*   Updated: 2025/09/29 17:17:02 by gastesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-typedef struct charset
+typedef struct rules
 {
 	char	empty;
 	char	filled;
 	char	obstacle;
-}	t_charset;
+	int	size;
+}	t_rules;
 
 #endif
