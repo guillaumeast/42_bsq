8509d858975229a235a5048c32b822216e14b1b2
Guillaume Astesana <gastesan@f1r10s15.clusters.42paris.fr>
2025-09-29 20:11:46 +0200

    add strndup before atoi in parse.c

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,67 +6,53 @@
 /*   By: adouieb <adouieb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:31:30 by adouieb           #+#    #+#             */
-/*   Updated: 2025/09/29 19:00:18 by gastesan         ###   ########.fr       */
+/*   Updated: 2025/09/29 20:11:12 by gastesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/parse.h"
 
-char	check_rules_format(char *str)
-{
-	if (ft_strlen(str) < 4)
-		return (0);
-	if (str[0] < '0' || str[0] > '9')
-		return (0);
-	while (str[0] >= '0' && str[0] <= '9')
-		str++;
-	if (ft_strlen(str) != 3)
-		return (0);
-	while (str[0] != '\0')
-	{
-		if (str[0] < 32 || str[0] > 126)
-			return (0);
-		str++;
-	}
-	return (1);
-}
-
 // Caller must free t_rules
 t_rules	*get_rules(char *str)
 {
 	t_rules	*rules;
+	int		size;
+	char	*number;
 
-	if (!check_rules_format)
+	if (ft_strlen(str) < 4)
 		return (NULL);
 	rules = malloc(sizeof(t_rules));
 	if (!rules)
 		return (NULL);
-	rules->size = atoi(str);
-	while (is_space(str[0]))
-		str++;
-	while (str[0] == '-' || str[0] == '+')
-		str++;
-	while (str[0] >= '0' && str[0] <= '9')
-		str++;
-	rules->empty = str[0];
-	rules->obstacle = str[1];
-	rules->filled = str[2];
+	size = ft_strlen(str);
+	number = strndup(str, size - 3);
+	if (!number)
+	{
+		ft_free_rules(&rules);
+		return (NULL);
+	}
+	rules->size = atoi(number);
+	free(number);
+	rules->empty = str[size - 3];
+	rules->obstacle = str[size - 2];
+	rules->filled = str[size - 1];
 	return (rules);
 }
-strndup str (size - 3)
 
 char	**parse(t_rules *rules, char *input)
 {
 	char	**lines;
-	t_rules	*rules;
 	int		i;
 
 	lines = ft_split(input, "\n");
-	if (!splitted)
-		return (NULL);
-	if (!check_rules(lines[0]))
+	if (!lines)
 		return (NULL);
 	rules = get_rules(lines[0]);
+	if (!rules)
+	{
+		// TODO : free lines
+		return (NULL);
+	}
 	// check each line validity
 	// return 0 + struct NULL if fail || 1 + struct if success
 }
