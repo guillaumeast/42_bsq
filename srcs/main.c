487aa17fa993c7aa34943eef3547b60cd5d67057
Alexander Douieb <adouieb@f1r10s14.clusters.42paris.fr>
2025-09-30 12:29:17 +0200

    change types for board

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: adouieb <adouieb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:31:04 by adouieb           #+#    #+#             */
-/*   Updated: 2025/09/30 00:11:09 by adouieb          ###   ########.fr       */
+/*   Updated: 2025/09/30 12:27:37 by adouieb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,16 @@ t_rules	init_rules()
 	return (rules);
 }
 
+t_run	*clean_rules(t_run *run_addr)
+{
+	run_addr->rules.width = 0;
+	run_addr->rules.height = 0;
+	run_addr->rules.empty = '\0';
+	run_addr->rules.obstacle = '\0';
+	run_addr->rules.filled = '\0';
+	return (run_addr);
+}
+
 t_cell	init_cell()
 {
 	t_cell	cell;
@@ -35,6 +45,14 @@ t_cell	init_cell()
 	return (cell);
 }
 
+t_run	*clean_cell(t_run *run_addr)
+{
+	run_addr->solution.last_best.x = 0;
+	run_addr->solution.last_best.y = 0;
+	run_addr->solution.last_best.value = 0;
+	return (run_addr);
+}
+
 t_solution	init_solution()
 {
 	t_solution	solution;
@@ -43,6 +61,42 @@ t_solution	init_solution()
 	solution.last_best = init_cell();
 	return (solution);
 }
+
+t_run	*clean_solution(t_run *run_addr)
+{
+	if (run_addr->solution.table != NULL)
+		ft_free_str_arr(&(run_addr->solution.table), -1);
+    return (run_addr);
+}
+
+t_run   *init_solution_table(t_run *run_addr)
+{
+	int			x;
+    int			y;
+    t_board_i	table;
+
+	if (run_addr->status == ERROR)
+		return (run_addr);
+	table = malloc(sizeof(t_board_i_row) * (height + 1));
+	if (table == NULL)
+		return (on_solution_table_failed(run_addr, NULL));
+	y = 0;
+	while (y < run->rules.height)
+	{
+		table[y] = malloc(sizeof(t_board_i_cell) * width);
+		if (table[y] == NULL)
+			return (on_solution_table_failed(run_addr, &table));
+		x = 0;
+		while (x < run->rules.width)
+			table[y][x++] = 0;
+		++y;
+    }
+	table[y] = NULL;
+    run_addr->solution.table = table;
+    return (run_addr);
+}
+
+
 
 t_run	init_run(t_filepath path)
 {
@@ -79,15 +133,17 @@ t_run	*from_stdin_to_runs()
 	return (file_to_run_map(paths, 1, from_file_to_run);
 }
 
-void	on_solution_table_failed(t_run run)
+t_run	*clean_rules(t_run *run_addr)
 {
-	
+	run_addr->rules.width = 0;
+	run_addr->rules.height = 0;
+	run_addr->rules.empty = '\0';
+	run_addr->rules.obstacle = '\0';
+	run_addr->rules.filled = '\0';
 }
 
-t_run	init_solution_table(t_run run)
+t_run	*on_solution_table_failed(t_run *run_addr, t_board *table)
 {
-	if (run.status == ERROR)
-		return (run);
 	
 }
 
