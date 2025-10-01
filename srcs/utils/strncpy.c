ed40b4da75f9f0401bae9164453c0b332fc3c9a8
Alexander Douieb <adouieb@f1r10s14.clusters.42paris.fr>
2025-10-01 13:07:24 +0200

    cleanup of utils folder files

--- srcs/utils/strncpy.c
+++ srcs/utils/strncpy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/bsq.h"
+#include "bsq.h"
 
 char	*ft_strncpy(char *dest, char *src, int size)
 {
