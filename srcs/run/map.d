b645097911008fb53a1eadc24f6a2cfd33274c2b
Guillaume Astesana <gastesan@f1r10s15.clusters.42paris.fr>
2025-10-01 22:22:59 +0200

    untracked test files

---
+++ srcs/run/map.d
@@ -0,0 +1,19 @@
+srcs/run/map.o: srcs/run/map.c includes/bsq.h includes/types.h \
+  includes/utils.h includes/algo.h includes/parse.h includes/print.h \
+  includes/read.h includes/run.h
+
+includes/bsq.h:
+
+includes/types.h:
+
+includes/utils.h:
+
+includes/algo.h:
+
+includes/parse.h:
+
+includes/print.h:
+
+includes/read.h:
+
+includes/run.h:
---
+++ srcs/utils/strncpy.d
@@ -0,0 +1,19 @@
+srcs/utils/strncpy.o: srcs/utils/strncpy.c includes/bsq.h \
+  includes/types.h includes/utils.h includes/algo.h includes/parse.h \
+  includes/print.h includes/read.h includes/run.h
+
+includes/bsq.h:
+
+includes/types.h:
+
+includes/utils.h:
+
+includes/algo.h:
+
+includes/parse.h:
+
+includes/print.h:
+
+includes/read.h:
+
+includes/run.h:
